4f2a026ef06ff3c2e354b7800750fe689ab126e5
Sebastien Barre <sebastien.barre@gmail.com>
2023-09-09 17:56:58 -0600

    step #3

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,41 @@
 import "./index.css";
 
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, from, gql } from "@apollo/client";
 
 import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const cache = new InMemoryCache();
-const client = new ApolloClient({ uri: import.meta.env.VITE_GRAPHQL_URI, cache });
+const httpLink = new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URI });
 
-const PING_QUERY = gql`
-  query {
-    ping
+const authRefreshMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: `Bearer ${import.meta.env.VITE_REFRESH_TOKEN}`,
+    },
+  }));
+  return forward(operation);
+});
+
+const refreshClient = new ApolloClient({ link: from([authRefreshMiddleware, httpLink]), cache });
+
+const SIGN_IN_MUTATION = gql`
+  mutation {
+    signIn {
+      token {
+        token
+      }
+    }
   }
 `;
 
-const { data } = await client.query({ query: PING_QUERY });
+const { data: signinData } = await refreshClient.mutate({ mutation: SIGN_IN_MUTATION });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <h1>{data.ping}</h1>
+    <h1>{signinData?.signIn?.token?.token?.substr(0, 10)}...</h1>
   </React.StrictMode>
 );
