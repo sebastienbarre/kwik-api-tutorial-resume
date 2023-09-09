1799a29bfee2aa5afd7ed4d7a1fe3411134ab35f
Sebastien Barre <sebastien.barre@gmail.com>
2023-09-09 11:18:59 -0600

    step #2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import "./index.css";
 
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
 import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const cache = new InMemoryCache();
+const client = new ApolloClient({ uri: import.meta.env.VITE_GRAPHQL_URI, cache });
+
+const PING_QUERY = gql`
+  query {
+    ping
+  }
+`;
+
+const { data } = await client.query({ query: PING_QUERY });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <h1>{data.ping}</h1>
   </React.StrictMode>
 );
