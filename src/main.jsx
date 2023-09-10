e42fb5f2907a6f4d7da2bbf25ad626c6899f23fc
Sebastien Barre <sebastien.barre@gmail.com>
2023-09-10 16:07:36 -0600

    step #4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import "./index.css";
 
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, from, gql } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  from,
+  gql,
+} from "@apollo/client";
 
 import App from "./App.jsx";
 import React from "react";
@@ -9,16 +17,18 @@ import ReactDOM from "react-dom/client";
 const cache = new InMemoryCache();
 const httpLink = new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URI });
 
-const authRefreshMiddleware = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      authorization: `Bearer ${import.meta.env.VITE_REFRESH_TOKEN}`,
-    },
-  }));
-  return forward(operation);
-});
+const makeAuthMiddleware = (token) =>
+  new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: `Bearer ${token}`,
+      },
+    }));
+    return forward(operation);
+  });
 
+const authRefreshMiddleware = makeAuthMiddleware(import.meta.env.VITE_REFRESH_TOKEN);
 const refreshClient = new ApolloClient({ link: from([authRefreshMiddleware, httpLink]), cache });
 
 const SIGN_IN_MUTATION = gql`
@@ -33,9 +43,11 @@ const SIGN_IN_MUTATION = gql`
 
 const { data: signinData } = await refreshClient.mutate({ mutation: SIGN_IN_MUTATION });
 
+const authMiddleware = makeAuthMiddleware(signinData.signIn.token.token);
+const client = new ApolloClient({ link: from([authMiddleware, httpLink]), cache });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <h1>{signinData?.signIn?.token?.token?.substr(0, 10)}...</h1>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
